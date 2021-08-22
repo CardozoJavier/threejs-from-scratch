549fd6090bd0ddc02e2e75da89c497e20bcc6656
Javier Cardozo <jcardozo@dlocal.com>
2021-08-22 00:29:06 -0300

    Initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import '../styles/sphere.css'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface PageProps {}
+interface MyAppProps {
+  Component: React.ComponentType;
+  pageProps: PageProps;
+}
+
+
+function MyApp({ Component, pageProps }: MyAppProps) {
+  return <Component {...pageProps} />;
 }
 export default MyApp
